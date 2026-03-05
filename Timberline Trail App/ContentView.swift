02900e4d55bbc7915c19c20c1a9cb34556b6d0c4
Michael Dankanich <michaeldankanich@gmail.com>
2026-03-05 12:24:05 -0800

    Initial Commit

---
+++ Timberline Trail App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timberline Trail App
+//
+//  Created by Michael Dankanich on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timberline Trail App/Timberline_Trail_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timberline_Trail_AppApp.swift
+//  Timberline Trail App
+//
+//  Created by Michael Dankanich on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Timberline_Trail_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
